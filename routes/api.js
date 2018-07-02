7511f80c721b859224bbe9b3635ab1fca99b16f0
Audrey <t.marzenia@gmail.com>
2018-07-02 22:09:04 +0200

    premier essai upload

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const cfg = require('../config/config');
+const cfg = require('../config/config');//path to the mongo connection
 const jwt = require('jsonwebtoken');//jsonwebtoken for authentication
 const User = require('../models/user')//call the Schema for a new user
 const mongoose = require('mongoose')
-const db = cfg.db//api for connecting the database with the admin users
+const db = "mongodb://Cotelette:a123456@ds141870.mlab.com:41870/socialnetwork"//cfg.db//api for connecting the database with the admin users
+const socket = require('socket.io')(server);
+const mongodb = require('mongodb');//call to store messages in the database
+
+///VARIABLES USED FOR THE CHAT APP///
+let users;
+let count;
+let chatRooms;
+let messagesArray = [];
+
 
 mongoose.connect(db, err => {
     if (err) {
@@ -12,7 +21,56 @@ mongoose.connect(db, err => {
     } else {
         console.log('connection to mongodb passed')//if the connection is alright, this is displayed into the terminal window
     }
-})
+
+  !  users = db.collection("users"); // getting the users collection
+  !  chatRooms = db.collection("chatRooms"); /* getting the chatRooms collection. This collection would store chats in that room*/
+
+  const io = socket.listen(server);//connection avec socket io
+  /* 'connection' is a socket.io event that is triggered when a new connection is
+     made. Once a connection is made, callback is called. */
+  io.sockets.on('connection', (socket) => { /* socket object allows us to join specific clients
+                                              to chat rooms and also to catch
+                                              and emit the events.*/
+      // 'join event'
+      socket.on('join', (data) => {
+          socket.join(data.room);
+          chatRooms.find({}).toArray((err, rooms) => {
+              if(err){
+                  console.log(err);
+                  return false;
+              }
+              count = 0;
+              rooms.forEach((room) => {
+                  if(room.name == data.room){
+                      count++;
+                  }
+              });
+              // Create the chatRoom if not already created
+              if(count == 0) {
+                  chatRooms.insert({ name: data.room, messages: [] });
+              }
+          });
+      });
+      // catching the message event
+      socket.on('message', (data) => {
+          // emitting the 'new message' event to the clients in that room
+          io.in(data.room).emit('new message', {user: data.user, message: data.message});
+          // save the message in the 'messages' array of that chat-room
+          chatRooms.update({name: data.room}, { $push: { messages: { user: data.user, message: data.message } } }, (err, res) => {
+              if(err) {
+                  console.log(err);
+                  return false;
+              }
+          });
+      });
+      // Event when a client is typing
+      socket.on('typing', (data) => {
+          // Broadcasting to all the users except the one typing
+          socket.broadcast.in(data.room).emit('typing', {data: data, isTyping: true});
+      });
+  });
+
+})//end connection to mongo db
 
 //function to verify the token send from the browser
 function verifyToken(req, res, next) {
@@ -40,6 +98,11 @@ router.get('/', (req, res) => {
 //HomePage routes
 router.get('/home', (req, res) => {
     res.send('Hi')
+})
+
+//Profil Route
+router.get('/profil', (req,res) => {
+  res.send('profil')
 })
 
 //Events route
--- server.js
+++ server.js
@@ -3,19 +3,42 @@ const express = require('express');
 const app = require('express')();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const upload = require('./upload');
 const port = 3000;
 const api = require('./routes/api')// call to the module for routes api.js
 const server = require('http').Server(app); //protocole http pour démarrer avec socket io
 const io = require('socket.io')(server);
+const mongodb = require('mongodb');//call to store messages in the database
+
+
+
 //const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api', api)
 app.get('/', function (req, res) {
-    res.send('Hello world')
+    res.send('Cotelette is here')
 });
+
+///CONFIGURATION FOR THE CHAT EXCHANGES///
+// Allowing cross-origin sites to make requests to this API
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin' , 'http://localhost:4200');
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append("Access-Control-Allow-Headers", "Origin, Accept,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.append('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+
+///CONFIGURATION TO UPLOAD FILES
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+app.post('/upload', upload);
 
 app.listen(port, function () {
     console.log('listening on port ' + port)
